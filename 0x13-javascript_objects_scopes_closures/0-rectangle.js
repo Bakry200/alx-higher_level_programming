fd05984e4da7940e6be0b4eb4385d5446cebfaed
Bakry200 <bakryalfadul@gmail.com>
2024-06-19 23:41:21 +0000

    sort

--- 0x13-javascript_objects_scopes_closures/0-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/0-rectangle.js
@@ -1,1 +1,11 @@
-cc
+#!/usr/bin/node
+/**
+ * This script defines an empty class Rectangle.
+ */
+
+class Rectangle {
+  // Empty class definition
+}
+
+// Export the Rectangle class to make it available for import in other modules
+module.exports = Rectangle;
